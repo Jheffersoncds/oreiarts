56fe1a8eb674ac1843f2bd989ecc1a4feed4e25a
Jheffersoncds <122843800+Jheffersoncds@users.noreply.github.com>
2023-01-25 02:04:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# oreiarts
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/oreiarts)
---
+++ src/defenitions/user.ts
@@ -0,0 +1,20 @@
+export interface User {
+  id: number;
+  name: string;
+  last_name: string;
+  date_of_birth: Date;
+  place_of_birth: string;
+  actual_position: Position;
+  avatar: Avatar;
+  heart: number;
+  awareness: number;
+  level: number;
+  dimension: Dimention;
+}
+export class user [
+  {
+    id: '1',
+    name: 'Jhefferson',
+    last_name: 'Castro Dos Santos',
+    date_of_birth: new Date
+]
